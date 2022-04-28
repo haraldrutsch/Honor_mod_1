1604a4f09cc67e1c9862d375e7d453be4f71e4a1
Harald Rutsch <h.rutsch@student.utwente.nl>
2022-04-28 09:41:40 +0200

    worked some more

--- main.py
+++ main.py
@@ -17,11 +17,9 @@ def coefficient(x, y):
     return a, b, c
 
 
-comb = permutations([2, 6, 4, 4, 6, 3, 2], 3)
+comb = permutations([3, 6, 4, 5, 6, 3, 2], 3)
 combArray = []
 for i in comb:
-    # print(i)
-    # print(numpy.asanyarray(i))
     combArray.append(numpy.asanyarray(i))
 
 comb2 = permutations([0, 1, 2, 3, 4, 5, 6], 3)
